f0dd6ab94f71d258718589e6ea0b9b79bc0de01b
John-Daly <johndaly1990@hotmai.com>
2022-08-24 17:48:20 +0200

    Added statement to break loop if duplicate is found

--- 217. Contains Duplicate/Solution.java
+++ 217. Contains Duplicate/Solution.java
@@ -1,14 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
-        Set<Integer> set = new HashSet<>();           //HashSet only stores non-duplicate values;
-        
-        for (int i = 0; i < nums.length; i++) {       
-            set.add(nums[i]);                         //Add elements of Array into HashSet
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
         }
-        
-        if (set.size() == nums.length){               //If HashSet size and array size are the same. There are no duplicates
-            return false;
-        } else return true;
+        return false;
     }
 }
